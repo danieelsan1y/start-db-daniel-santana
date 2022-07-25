2b842e9a8bf9d29bdea9a982300530cf51307836
Daniel Santana <danieelsan1y@gmail.com>
2022-07-24 22:54:19 -0300

    final

--- validacao/validar-etapa.js
+++ validacao/validar-etapa.js
@@ -3,14 +3,6 @@ const validarEtapa = (vidasEsperadas, palavraEsperada, letrasChutadasEsperadas, 
    const palavra = arrPalavra.join('');
     const letrasChutadas = arrLetrasChutadas.join('');
     const estado = jogoForca.buscarEstado();
-    console.log(arrPalavra)
-    console.log( jogoForca.buscarDadosDoJogo())
-    console.log(jogoForca.buscarEstado())
-    console.log('vidas === vidasEsperadas', vidas === vidasEsperadas)
-    console.log('palavra === palavraEsperada', palavra === palavraEsperada)
-    console.log('letrasChutadas === letrasChutadasEsperadas', letrasChutadas === letrasChutadasEsperadas)
-    console.log('estado === estadoEsperado', estado === estadoEsperado)
-
 
     return vidas === vidasEsperadas
         && palavra === palavraEsperada
